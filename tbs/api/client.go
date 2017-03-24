6f1906035129152b4feaa75ab08cfc251fc42d12
Jan Kowalski <jankowalskipraca85@gmail.com>
2017-03-24 14:06:37 +0100

    Change api root to url from domain
    Signed-off-by: Jan Kowalski <jankowalskipraca85@gmail.com>

--- tbs/api/client.go
+++ tbs/api/client.go
@@ -1,16 +1,24 @@
 package api
 
 import (
+	"net/url"
+
+	apiclient "github.com/3Blades/go-sdk/client"
 	httptransport "github.com/go-openapi/runtime/client"
 	"github.com/go-openapi/strfmt"
-	apiclient "github.com/3Blades/go-sdk/client"
+	jww "github.com/spf13/jwalterweatherman"
 	"github.com/spf13/viper"
 )
 
 func Client() *apiclient.Threeblades {
 	apiRoot := viper.GetString("root")
+	root, err := url.Parse(apiRoot)
+	if err != nil {
+		jww.ERROR.Fatal("You need to provide valid url as api root.")
+		return nil
+	}
 	token := viper.GetString("token")
-	transport := httptransport.New(apiRoot, "", []string{"http"})
+	transport := httptransport.New(root.Host, "", []string{root.Scheme})
 	if token != "" {
 		transport.DefaultAuthentication = httptransport.APIKeyAuth("AUTHORIZATION", "header", "Token "+token)
 	}
