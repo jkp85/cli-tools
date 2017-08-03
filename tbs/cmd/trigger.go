c4e11105aff79d7ddd3226271bbaa38964bdb164
Jan Kowalski <jankowalskipraca85@gmail.com>
2017-08-03 16:25:22 +0200

    Add trigger command
    Signed-off-by: Jan Kowalski <jankowalskipraca85@gmail.com>

--- tbs/cmd/trigger.go
+++ tbs/cmd/trigger.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"errors"
-
 	"github.com/3Blades/cli-tools/tbs/api"
 	"github.com/3Blades/go-sdk/client/triggers"
 	"github.com/3Blades/go-sdk/models"
@@ -14,6 +12,7 @@ func init() {
 	cmd := triggerCmd()
 	cmd.AddCommand(
 		sendSlackMessage(),
+		createTriggerCmd(),
 	)
 	RootCmd.AddCommand(cmd)
 }
@@ -79,28 +78,42 @@ func sendSlackMessage() *cobra.Command {
 	return cmd
 }
 
-func validateTriggerAction(ta *models.TriggerAction) error {
-	if ta.ActionName == nil {
-		return errors.New("no action name")
-	}
-	if ta.Method == nil {
-		return errors.New("no method")
-	}
-}
-
 func createTriggerCmd() *cobra.Command {
-	cause := &models.TriggerAction{}
-	effect := &models.TriggerAction{}
-	webhook := &models.Webhook{}
+	cause := &models.TriggerAction{
+		ActionName: new(string),
+		Method:     new(string),
+	}
+	effect := &models.TriggerAction{
+		ActionName: new(string),
+		Method:     new(string),
+	}
+	webhook := &models.Webhook{
+		URL: new(string),
+	}
+	causePayload := api.NewJSONVal()
+	effectPayload := api.NewJSONVal()
+	webhookConfig := api.NewJSONVal()
 	schedule := ""
 	body := &models.TriggerData{}
 	cmd := &cobra.Command{
 		Use:   "create",
 		Short: "Create trigger",
-		PreRunE: func(cmd *cobra.Command, args []string) error {
-
-		},
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if *effect.ActionName != "" && *effect.Method != "" {
+				body.Effect = effect
+				body.Effect.Payload = effectPayload.Value
+			}
+			if *cause.ActionName != "" && *cause.Method != "" {
+				body.Cause = cause
+				body.Cause.Payload = causePayload.Value
+			}
+			if *webhook.URL != "" {
+				body.Webhook = webhook
+				body.Webhook.Config = webhookConfig.Value
+			}
+			if schedule != "" {
+				body.Schedule = schedule
+			}
 			resp, err := createTrigger(body)
 			if err != nil {
 				return err
@@ -113,14 +126,14 @@ func createTriggerCmd() *cobra.Command {
 	flags.StringVar(cause.Method, "cause-method", "", "Cause method")
 	flags.StringVar(&cause.Model, "cause-model", "", "Cause type")
 	flags.StringVar(&cause.ObjectID, "cause-object", "", "Cause object")
-	flags.StringVar(&cause.Payload, "cause-payload", "", "Cause payload")
+	flags.VarP(causePayload, "cause-payload", "", "Cause payload")
 	flags.StringVar(effect.ActionName, "effect-action", "", "Effect action")
 	flags.StringVar(effect.Method, "effect-method", "", "Effect method")
 	flags.StringVar(&effect.Model, "effect-model", "", "Effect type")
 	flags.StringVar(&effect.ObjectID, "effect-object", "", "Effect object")
-	flags.StringVar(&effect.Payload, "effect-payload", "", "Effect payload")
+	flags.VarP(effectPayload, "effect-payload", "", "Effect payload")
 	flags.StringVar(webhook.URL, "webhook-url", "", "Webhook url")
-	flags.StringVar(webhook.Config, "webhook-congig", "", "Webhook config")
+	flags.VarP(webhookConfig, "webhook-congig", "", "Webhook config")
 	flags.StringVar(&schedule, "schedule", "", "Cron schedule")
 	return cmd
 }
